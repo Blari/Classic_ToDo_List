56f05e730421b496c16ad00fa213b1687b435324
Blari <Blarior@gmail.com>
2019-11-06 00:16:18 +0300

    morecode )

--- app.js
+++ app.js
@@ -32,5 +32,48 @@ const tasks = [
 ];
 
 (function(arrOfTasks) {
-    console.log(arrOfTasks);
+    const objectOfTasks = arrOfTasks.reduce((acc, task) => {
+        acc[task._id] = task;
+        return acc;
+    }, {});
+
+    renderAllTasks(objectOfTasks);
+
+    function renderAllTasks(tasksList) {
+        if (!tasksList) {
+            console.error("Передайте список задач");
+            return;
+        }
+
+        const fragment = document.createDocumentFragment();
+
+        Object.values(tasksList).forEach(task => {
+            const li = listItemTemplate(task);
+        });
+    }
+
+    function listItemTemplate({ _id, title, body } = {}) {
+        const li = document.createElement("li");
+        li.classList.add(
+            "list-group-item",
+            "d-flex",
+            "align-items-center",
+            "flex-wrap",
+            "mt-2"
+        );
+        const span = document.createElement("span");
+        span.textContent = title;
+        span.style.fontWeight = "bold";
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete task";
+        deleteBtn.classList.add("btn", "btn-danger", "ml-auto", "delete-btn");
+
+        const article = document.createElement("p");
+        article.textContent = body;
+        article.classList.add("mt-2", "w-100");
+        li.appendChild(span);
+        li.appendChild(deleteBtn);
+        li.appendChild(article);
+    }
 })(tasks);
